c5aa584704836bdde84cbee564a822762c100b77
Luiz Antonio <luiz8907@hotmail.com>
2022-02-20 10:48:23 -0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,7 +49,7 @@ class UserController extends Controller
         // })
         // ->orderBy('users.id', 'ASC')
         // ->get();
-        
+
         // echo "<h2>Total de registros : {$users->count()}</h2>";
         // foreach ($users as $user) {
         //     echo "<p>#{$user->id} - <strong>Nome:</strong>{$user->name} - {$user->status} - {$user->address}</p>";
@@ -62,17 +62,26 @@ class UserController extends Controller
         //     'email' => 'luiz@luiz.com.br',
         //     'password' => bcrypt('#ç]W%@@+LuIz?'),
         //     'status' => '1'
-            
+
 
         // ]);
 
         //atualizando um registro na tabela, ATENÇÃO, O UPDATE É FEITO SEMPRE COM O USO DO WHERE
         // DB::table('users')->where('id', '=', '1001')->update([
         //     'name' => 'Luiz Antonio ',
-            
+
         // ]);
 
         //deletando um valor na tabela
-        DB::table('users')->where('id', '=', '1001')->delete();
+        //DB::table('users')->where('id', '=', '1001')->delete();
+
+        $users = DB::table('users')->paginate(10);
+
+        foreach ($users as $user) {
+            echo "<p>#{$user->id} - <strong>Nome:</strong>{$user->name} - {$user->status}</p>";
+        }
+        echo "<hr>";
+
+        echo $users->links();
     }
 }
